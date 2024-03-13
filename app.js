b699b3f1576d3a547540aecc919872074170b3ad
ANSHU TOPPO <105590074+toppo04@users.noreply.github.com>
2024-03-14 00:21:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+
+const questions = [{
+    'que':'Which of the following is the markup language',
+    'a':'HTML',
+    'b':'CSS',
+    'c':'JavaScript',
+    'd':'PHP',
+    'correct': 'a'
+},
+
+{
+    'que':'When was JavaScript launched',
+    'a':'1995',
+    'b':'1996',
+    'c':'1994',
+    'd':'None of the Above',
+    'correct':'a'
+},
+
+{
+    'que':'CSS stands for?',
+    'a':'Hypertext Markup language',
+    'b':'Cascade Style Sheet',
+    'c':'Cascading Style Sheet',
+    'd':'None of the above',
+    'correct':'c',
+}
+]
+
+
+let index = 0;
+let correct = 0,
+    incorrect = 0,
+    total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+    if (total === index) {
+        return quizEnd()
+    }
+    reset()
+    const data = quizData[index]
+    questionBox.innerHTML = `${index + 1}) ${data.question}`
+    allInputs[0].nextElementSibling.innerText = data.a
+    allInputs[1].nextElementSibling.innerText = data.b
+    allInputs[2].nextElementSibling.innerText = data.c
+    allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+    "click",
+    function() {
+        const data = quizData[index]
+        const ans = getAnswer()
+        if (ans === data.correct) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+        index++;
+        loadQuestion()
+    }
+)
+
+const getAnswer = () => {
+    let ans;
+    allInputs.forEach(
+        (inputEl) => {
+            if (inputEl.checked) {
+                ans = inputEl.value;
+            }
+        }
+    )
+    return ans;
+}
+
+const reset = () => {
+    allInputs.forEach(
+        (inputEl) => {
+            inputEl.checked = false;
+        }
+    )
+}
+
+const quizEnd = () => {
+    // console.log(document.getElementsByClassName("container"));
+    document.getElementsByClassName("container")[0].innerHTML = `
+        <div class="col">
+            <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+        </div>
+    `
+}
+loadQuestion(index);
+
+
+
+
+
+
+
+
+
